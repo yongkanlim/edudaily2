a3cd866359c5698249f41d1d767c4532ec10a44c
yongkan <yongkanlim@gmail.com>
2026-01-18 13:56:33 +0800

    remove base in vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',  // <-- ADD THIS
+  // base: './',  // <-- ADD THIS
   plugins: [
     tailwindcss(),
     react()
